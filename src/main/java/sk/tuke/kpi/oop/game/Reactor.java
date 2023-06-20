a918c09857921c67de908fa771227092afc912f5
zamboripeter <zambori@spseke.sk>
2023-06-20 13:05:48 +0200

    trieda Reactor v2 temperature, damage, image

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -11,10 +11,47 @@ public class Reactor extends AbstractActor {
     private int damage;
     private Animation normalAnimation;
     public Reactor(){
-        temperature = 0;
-        state = false;
-        damage = 0;
-        normalAnimation = new Animation("sprites/reactor_on.png", 80,80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
-        setAnimation(normalAnimation);
+        this.temperature = 0;
+        this.state = false;
+        this.damage = 0;
+
+        this.normalAnimation = new Animation(
+            "sprites/reactor_on.png",
+            80,
+            80,
+            0.1f,
+            Animation.PlayMode.LOOP_PINGPONG);
+        setAnimation(this.normalAnimation);
+
+    }
+
+
+    public int getTemperature(){
+        return this.temperature;
+    };
+    public int getDamage(){
+        return this.damage;
+    }
+    public void increaseTemperature(int increment){
+        this.temperature += increment;
+        if (this.temperature >= 2000) {
+            if (this.temperature >= 6000) {
+                this.damage = 100;
+                return;
+            }
+        }
+        int damage = (getTemperature()/40) - 50;
+
+        if (this.damage < damage){
+            this.damage = damage;
+             }
+        if (this.temperature > 4000){
+            setAnimation(new Animation(
+                "sprites/reactor_hot.png",
+                80,
+                80,
+                0.01f,
+                Animation.PlayMode.LOOP_PINGPONG));
+        }
     }
 }
