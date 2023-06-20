676435fe4465265d327c2b7f80664030a7dfa3cb
zamboripeter <zambori@spseke.sk>
2023-06-20 11:32:56 +0200

    trieda Reactor v1

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -1,5 +1,20 @@
 package sk.tuke.kpi.oop.game;
 
-public class Reactor extend Actor {
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
+import sk.tuke.kpi.gamelib.graphics.Animation;
 
+//potomok ma rozsirene vlastnosti rodica
+public class Reactor extends AbstractActor {
+    private int temperature;
+    private boolean state;
+    private int damage;
+    private Animation normalAnimation;
+    public Reactor(){
+        temperature = 0;
+        state = false;
+        damage = 0;
+        normalAnimation = new Animation("sprites/reactor_on.png", 80,80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        setAnimation(normalAnimation);
+    }
 }
